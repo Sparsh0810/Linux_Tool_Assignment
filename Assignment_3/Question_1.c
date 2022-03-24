b1bb1897f72947e1a9c035dc5723ce203d7a6a7e
Sparsh Jain <77033352+Sparsh0810@users.noreply.github.com>
2022-03-24 23:48:52 +0530

    Create Question_1.c

---
+++ Assignment_3/Question_1.c
@@ -0,0 +1,50 @@
+//Sparsh Jain
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<pthread.h>
+
+pthread_t tid1, tid2;		
+
+int globalVar = 10;		
+
+
+//increase globalVar by 15
+void* global_increase(void *arg)
+{
+	printf("1st thread in execution\n");
+	globalVar += 15;
+	printf("after increment : %d\n\n", globalVar);
+	
+	return NULL;
+}
+
+
+//decrease globalVar by 5
+void* global_decrease(void *arg)
+{
+	printf("2nd thread in execution\n");
+	globalVar -= 5;
+	printf("after decrement : %d\n", globalVar);
+	
+	return NULL;
+}
+
+
+int main(void)
+{
+	pthread_create(&tid1, NULL, global_increase, NULL);
+	pthread_create(&tid2, NULL, global_decrease, NULL);
+	
+	//executing threads
+	pthread_join(tid1, NULL);
+	pthread_join(tid2, NULL);
+	
+	//returning to parent process
+	printf("\nExecuting parent process\n");
+	
+	return 0;
+}
+
+	
